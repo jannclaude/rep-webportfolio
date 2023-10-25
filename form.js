a0f828a656b631b2921844079aea28bfe7899fcb
jannclaudejesus <jannclaudejesus@gmail.com>
2023-10-25 21:00:47 +0800

    update html/css, add form.js

---
+++ form.js
@@ -0,0 +1,37 @@
+var sendButton = document.getElementById('contact-link');
+
+sendButton.addEventListener('click', function() {
+
+	var newName = document.getElementById("exampleFormControlInput1").value,
+		newEmail = document.getElementById("exampleFormControlInput2").value,
+		newIcon = document.getElementById("floatingSelect1").value,
+		newSocialName = document.getElementById("basic-addons1").value,
+		newSubject = document.getElementById("floatingSelect").value,
+		newMessage = document.getElementById("exampleFormControlTextarea1").value
+
+		subject = newSubject,
+		emailAddr = 'jannclaudejesus@gmail.com';
+
+		if (!newSocialName) {
+			queryString = '?subject=' + subject + ' from ' + newEmail + "&body=Hello, I'm " + newName + '. ' + newMessage
+		} else {
+			queryString = '?subject=' + subject + ' from ' + newEmail + "&body=Hello, I'm " + newName + '. ' + newMessage + '\n\nYou could also reach me through my ' + newIcon + " account @" + newSocialName
+		}
+
+		this.href = window.encodeURI('mailto: ' + emailAddr + queryString);
+
+		console.log(this.href);
+})
+
+function stoppedTyping() {
+	var input1 = document.getElementById("exampleFormControlInput1"),
+		input2 = document.getElementById("exampleFormControlInput2"),
+		input3 = document.getElementById("exampleFormControlTextarea1"),
+		link = document.getElementById("contact-link")
+
+	if (input1.value.trim() !== '' && input2.value.trim() !== '' && input3.value.trim() !== '') {
+		link.classList.remove('disabled');
+	} else {
+		link.classList.add('disabled');
+	}
+}
