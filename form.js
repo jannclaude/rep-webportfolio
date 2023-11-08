030e38dfa0217fd77edc6a1212e77c8ce640c952
jannclaude <jannclaudejesus@gmail.com>
2023-11-08 23:04:25 +0800

    debug 16

--- form.js
+++ form.js
@@ -40,7 +40,7 @@ function updateViewport() {
 	const metaTag = document.querySelector('meta[name="viewport"]');
 	
 	if (window.matchMedia('(max-width: 992px)').matches) {
-	  metaTag.setAttribute('content', 'width=device-width, initial-scale=0.95');
+	  metaTag.setAttribute('content', 'width=device-width, initial-scale=0.9');
 	} else {
 	  metaTag.setAttribute('content', 'width=device-width, initial-scale=1');
 	}
